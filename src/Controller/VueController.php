72b3781de7fdec22638c51d503e1cdc33d578bdd
Terqaz <holinvova@gmail.com>
2024-06-02 01:01:38 +0300

    fix: поправил VueController

--- src/Controller/VueController.php
+++ src/Controller/VueController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController extends AbstractController
+class VueController extends AbstractController
 {
     /**
      * Отправляем все остальные запросы, которые не подошли ни под один роут, во vue-router
